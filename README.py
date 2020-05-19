2805754100f53dd56c996e2b47ba29130f0d40d9
Vedang2805 <57453717+Vedang2805@users.noreply.github.com>
2020-05-19 18:09:48 +0530

    Update README.py

--- README.py
+++ README.py
@@ -1,6 +1,16 @@
+import math
 inp=input("Enter required conversion")
 if inp="Cartesian to Polar" :
    x=float(input("Enter x co-ordinat:")
    y=float(input("Enter y co-ordinate:")
-   r=((x**2) +
+   r=((x**2) + (y**2))**(1/2)
+   a=math.atan((y/x))
+   print("The polar co-ordinates are:",r,a)
+elif inp="Polar to Cartesian" :
+   r=float(input("Enter length from origin:"))
+   a=float(input("Enter angle made with x-axis:))
+   x=r*(math.cos(a))
+   y=r*(math.sin(a))
+   print("The Cartesian co-ordinates are:",x,y)    
+           
    
